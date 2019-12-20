78bfe1269d5bc5fc038f4ce9987d8f8b698d84e6
True Ghiassi <true@ghiassitrio.co.uk>
2019-12-20 18:05:21 +0000

    Removed some more junk files.

--- copyData.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -rv dataFiles/ true@whatthestudentsthink.uk:./wtst/dataFiles
--- copyServer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync .stack-work/install/x86_64-linux/lts-12.5/8.4.3/bin/server true@whatthestudentsthink.uk:./wtst/
