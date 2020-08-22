6edd40cc6c0361715b566da81a6bb949027d36ef
True Ghiassi <true@ghiassitrio.co.uk>
2020-08-22 22:47:29 +0100

    Removed some trailing whitespace.

--- haskell/src/MakeElm.hs
+++ haskell/src/MakeElm.hs
@@ -5,13 +5,13 @@
 -- Whatthestudentsthink is free software: you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License as
 -- published by the Free Software Foundation, either version 3 of the
--- License, or (at your option) any later version.  
--- 
+-- License, or (at your option) any later version.
+--
 -- Whatthestudentsthink is distributed in the hope that it will be
 -- useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 -- of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 -- General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU General Public License
 -- along with Whatthestudentsthink.  If not, see
 -- <http://www.gnu.org/licenses/>.
@@ -171,7 +171,7 @@ oneNss3ToInt uniMap subjMap line =
 
         _ ->
             Nothing
-  
+
 
 {-| It takes in a list of all the larger university names and the
 whole of the parsed data from the 'NSS' worksheet, and removes all
