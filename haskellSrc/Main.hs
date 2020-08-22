f282375ea0e925073e44fc5c1e27fce49522b619
True Ghiassi <true@ghiassitrio.co.uk>
2020-08-22 20:35:01 +0100

    Revert "Tidied up Main, but still broken."
    This reverts commit 54a8ea4f65f4b57ba69c1fdf16211ab3fe4668e7.

--- haskellSrc/Main.hs
+++ haskellSrc/Main.hs
@@ -1,16 +1,37 @@
+-- Copyright 2017 True Ghiassi true@ghiassitrio.co.uk
+
+-- This file is part of Whatthestudentsthink.
+
+-- Whatthestudentsthink is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.  
+-- 
+-- Whatthestudentsthink is distributed in the hope that it will be
+-- useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+-- of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with Whatthestudentsthink.  If not, see
+-- <http://www.gnu.org/licenses/>.
+
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+{-| This module analyses the data files and creates the Elm code
+containing the various lookup dictionaries for the front end.
+-}
+
 import qualified Data.Text.IO as Tio
-import qualified Data.ByteString as B
-import MakeElm (makeElm)
+import MakeElmCode (elmify)
+import ReadAndBasicProcess (readAndBasicProcess)
 
 main :: IO ()
 main = do
-    nssRaw <- B.readFile "nss.csv"
-    nss3Raw <- B.readFile "nss3.csv"
-    case makeElm nssRaw nss3Raw of
-        Left err ->
-            Tio.putStrLn err
-
-        Right elm ->
-            Tio.writeFile "elm/src/Data.elm" elm
+  eitherResult <- readAndBasicProcess
+  case eitherResult of
+    Left err -> print err
+    Right result ->
+      Tio.writeFile "elmSrc/Data.elm" $ elmify result
